0c135c1e5830710bb0f3c5d580c0c4cf5108ab3e
Mohieb <dev.mohieb@gmail.com>
2023-04-25 21:49:43 +0300

    set up and render components

--- digital-business-card/src/App.jsx
+++ digital-business-card/src/App.jsx
@@ -1,11 +1,16 @@
-import './App.css'
+import "./App.css";
+import Header from "./components/Header";
+import Main from "./components/Main";
+import Footer from "./components/Footer";
 
 function App() {
   return (
-    <>
-      
-    </>
-  )
+    <div className="flex max-w-[320px] flex-col items-center justify-center overflow-hidden rounded-xl">
+      <Header />
+      <Main />
+      <Footer />
+    </div>
+  );
 }
 
-export default App
+export default App;
